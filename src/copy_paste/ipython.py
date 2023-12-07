06ef441f23ce8ceb50fffb92cda270601f912063
Jason Wilkes <notarealdeveloper@gmail.com>
2023-12-07 14:19:50 -0800

    Resolve some ipython namespacing problems

--- src/copy_paste/ipython.py
+++ src/copy_paste/ipython.py
@@ -2,68 +2,33 @@
 
 __all__ = [
     'install',
-    'install_xc',
-    'install_xv',
 ]
 
+from IPython import get_ipython # necessary!
+
+from IPython.core.magic import register_line_cell_magic
 
 def install():
-    install_xc()
-    install_xv()
+    @register_line_cell_magic
+    def xc(line, cell=None):
+        import subprocess
+        if cell is None:
+            # called as %xc (line magic)
+            data = line
+        else:
+            # called as %%xc (cell magic)
+            data = re.sub('\n+$', '\n', cell)
 
-def install_xc():
-    from IPython.core.magic import register_line_cell_magic
-    return register_line_cell_magic(xc)
+        s = data.encode()
 
-def install_xv():
-    from IPython.core.magic import register_line_cell_magic
-    return register_line_cell_magic(xv)
+        p = subprocess.Popen(
+            ['xclip', '-selection', 'p'],
+            stdin = subprocess.PIPE,
+        )
+        p.communicate(s)
 
-def xc(line, cell=None):
-    import subprocess
-    if cell is None:
-        # called as %xc (line magic)
-        data = line
-    else:
-        # called as %%xc (cell magic)
-        data = re.sub('\n+$', '\n', cell)
-
-    s = data.encode()
-
-    p = subprocess.Popen(
-        ['xclip', '-selection', 'p'],
-        stdin = subprocess.PIPE,
-    )
-    p.communicate(s)
-
-    p = subprocess.Popen(
-        ['xclip', '-selection', 'c'],
-        stdin = subprocess.PIPE,
-    )
-    p.communicate(s)
-
-
-def xv(line, cell=None):
-    import subprocess
-    if cell is None:
-        # called as %xc (line magic)
-        data = line
-    else:
-        # called as %%xc (cell magic)
-        data = re.sub('\n+$', '\n', cell)
-
-    data = str(eval(data))
-
-    s = data.encode()
-
-    p = subprocess.Popen(
-        ['xclip', '-selection', 'p'],
-        stdin = subprocess.PIPE,
-    )
-    p.communicate(s)
-
-    p = subprocess.Popen(
-        ['xclip', '-selection', 'c'],
-        stdin = subprocess.PIPE,
-    )
-    p.communicate(s)
+        p = subprocess.Popen(
+            ['xclip', '-selection', 'c'],
+            stdin = subprocess.PIPE,
+        )
+        p.communicate(s)
